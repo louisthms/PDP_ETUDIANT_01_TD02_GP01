c75d6a8b123af5d152612031e58c0c91f545f559
louisthms <louistwin78thms@gmail.com>
2023-01-10 15:00:06 +0100

    etape-3

--- PDP_BREADBOARD/src/main.cpp
+++ PDP_BREADBOARD/src/main.cpp
@@ -6,30 +6,41 @@
 int LED = 26;
 const byte DHTPIN = 33;
 // Definit le type de capteur utilise
-DHT dht(DHTPIN, DHT11);
+DHT dht(DHTPIN, DHT22);
 
 void setup()
 {
+
+  // Configuration timer 5 seconds
+  Serial.println();
+
   // put your setup code here, to run once:
   Serial.begin(9600);
   dht.begin();
   pinMode(LED, OUTPUT);
-}
 
-void loop()
-{
-
-  digitalWrite(LED, HIGH);
+  // code pour la led
+  /*  digitalWrite(LED, HIGH);
   delay(200);
   digitalWrite(LED, LOW);
-  delay(200);
+  delay(200);*/
+
   // put your main code here, to run repeatedly:
 
+  // sensor initialize and display (DHT)
   int TemperatureSensor = dht.readTemperature();
   int HumiditySensorAir = dht.readHumidity();
 
   Serial.println("Temperature = " + String(dht.readTemperature()) + " °C");
   Serial.println("Humidite = " + String(dht.readHumidity()) + " %");
+  Serial.println("mode veille de 5 secondes");
 
-  delay(5000);
+  // Deep sleep mode
+  Serial.println("Deep sleep mode");
+  delay(100);
+  ESP.deepSleep(5e6);
+}
+
+void loop()
+{
 }
